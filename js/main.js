073a41628a1092658d54a2ee8def8e3837cdc770
Amisha Patel <amishapatel@Amishas-MBP.T-mobile.com>
2017-07-19 18:56:37 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 
 //console.log("Up and running!");
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
-var cardFour = "king";
+var cardFour = "king";*/
+
+var cards = ["queen","king","queen","king"];
+var cardsInPlay=[];
+
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+
+var cardTwo = cards[1];
+cardsInPlay.push('cardTwo');
+
+if(cardsInPlay.length === 2)
+{
+	if(cardsInPlay[0]===cardsInPlay[1])
+	{
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry! Try again!");
+	}
+}
 
 console.log("User flipped" + " " +cardOne);
 console.log("User flipped" + " " +cardTwo);
